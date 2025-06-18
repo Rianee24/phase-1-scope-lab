51f47d52c2452b4c006fe349db34a10bcbd69381
Ainley Riany <ainley.riany@student.moringaschool.com>
2025-06-18 09:47:20 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function customerName() {
+
+}
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavoriteCustomer = "not bob";
+    leastFavoriteCustomer = "maybe bob"; 
+}
